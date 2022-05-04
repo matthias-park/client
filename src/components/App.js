3639f6bca284da1495603c918c864eb6e9e68878
Junsoo Park <matthiaspark0514@gmail.com>
2022-05-04 09:58:53 -0700

    Adding Header and Connect Route

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,23 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-
-const PageOne = () => {
-    return <div>PageOne
-        <Link to="/pageTwo">Navigate to Page two</Link>
-    </div>
-}
-
-const PageTwo = () => {
-    return <div>PageTwo
-        <Link to="/">Navigate to Page One</Link>
-    </div>
-}
+import StreamList from "./streams/StreamList";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamShow from "./streams/StreamShow";
+import StreamCreate from "./streams/StreamCreate";
+import Header from "./header";
 
 const App = () => {
     return (
-        <div>
+        <div className="">
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={PageOne} />
-                    <Route path="/pageTwo" component={PageTwo} />
+                    <Header />
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/stream/new" exact component={StreamCreate} />
+                    <Route path="/stream/edit" exact component={StreamEdit} />
+                    <Route path="/stream/delete" exact component={StreamDelete} />
+                    <Route path="/stream/show" exact component={StreamShow} />
                 </div>
             </BrowserRouter>
         </div>
