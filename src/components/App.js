4a0b5dcaca2f745e260df88cb4a8b91742f62fc1
Junsoo Park <matthiaspark0514@gmail.com>
2022-05-03 20:56:57 -0700

    update React Router, Link

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const PageOne = () => {
+    return <div>PageOne
+        <Link to="/pageTwo">Navigate to Page two</Link>
+    </div>
+}
+
+const PageTwo = () => {
+    return <div>PageTwo
+        <Link to="/">Navigate to Page One</Link>
+    </div>
+}
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/pageTwo" component={PageTwo} />
+                </div>
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App;
