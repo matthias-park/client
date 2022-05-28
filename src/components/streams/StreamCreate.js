4a109a88e2a91173ae6ce8788b050d274c5bb798
Junsoo Park <matthiaspark0514@gmail.com>
2022-05-28 15:16:08 -0700

    add redux-thunk for async api calling

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import {connect} from 'react-redux';
+import { createStream} from '../../actions'
+import { formValues } from "redux-form";
 
 class StreamCreate extends React.Component {
     renderError({error, touched}) {
@@ -24,8 +27,8 @@ class StreamCreate extends React.Component {
         
     }
 
-    onSubmit(event) {
-        event.preventDefault();
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues)
     }
 
     render() {  
@@ -33,6 +36,7 @@ class StreamCreate extends React.Component {
             <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
                 <Field name="title" component={this.renderInput} label="Enter Title"/>
                 <Field name="description" component={this.renderInput} label="Enter Description"/>
+                <button className="ui button primary">Submit</button>
             </form>
         )
     }
@@ -51,7 +55,9 @@ const validate = (formValues) => {
     return errors;
 }
 
-export default reduxForm({
+const fromWrapped = reduxForm({
     form: 'streamCreate',
     validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(fromWrapped)
